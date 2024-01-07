5c278f5d5a1257174eaa278a67d868c167750a92
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-01-07 17:32:28 +0700

    Implement authorization middleware to verify user

---
+++ internal/http/middleware/authorization.go
@@ -0,0 +1,61 @@
+package middleware
+
+import (
+	"errors"
+	"fmt"
+	"fund-o/api-server/pkg/token"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+
+	AuthorizationPayloadKey = "authorization_payload"
+)
+
+func AuthMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authorizationHeader := c.GetHeader(authorizationHeaderKey)
+		if len(authorizationHeader) == 0 {
+			err := errors.New("authorization header is not provided")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) < 2 {
+			err := errors.New("invalid authorization header format")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != authorizationTypeBearer {
+			err := fmt.Errorf("unsupported authorization type %s", authorizationType)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		c.Set(AuthorizationPayloadKey, payload)
+		c.Next()
+	}
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{
+		"status":      http.StatusText(http.StatusUnauthorized),
+		"status_code": http.StatusUnauthorized,
+		"error":       err.Error(),
+	}
+}
